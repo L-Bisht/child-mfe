31f7cb52a61916ff0edeb2286d353945f5ae96dd
Lalit Singh Bisht <52027859+L-Bisht@users.noreply.github.com>
2025-11-27 19:12:16 +0000

    feat: enhance App component layout and styling, remove unused CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="flex justify-center items-center">
-      <h5 className="text-4xl">This is child mfe</h5>
+    <main className="flex justify-center items-center w-full h-full bg-red-500">
+      <h5 className="text-4xl ">This is child mfe</h5>
+      <h1 className="text-9xl font-extrabold">Micro frontend</h1>
     </main>
   );
 }
